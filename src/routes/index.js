0d20ca1d275b79f4fb482fa794d3b804fd04dc41
Otis <ledat02072005@gmail.com>
2025-11-23 23:20:08 +0700

    s

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,7 +1,13 @@
 import AuthRouter from './auth.js';
 export default function routes(app) {
     // Define your routes here
-
+    // Thêm route gốc để kiểm tra tình trạng server
+    app.get('/', (req, res) => {
+        res.status(200).json({ 
+            message: 'Backend API is running!',
+            environment: process.env.NODE_ENV || 'development'
+        });
+    });
     app.use('/api/auth', AuthRouter)
     app.use('/api', (req, res) => {
         res.send('API is working');
