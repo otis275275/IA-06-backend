7877fed53fb7555305ef684142bace72a64ecdd5
Otis <ledat02072005@gmail.com>
2025-11-24 09:52:40 +0700

    s

--- src/routes/home.js
+++ src/routes/home.js
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 const router = Router();
 
-router.get('/', (req, res, next) => {
+router.get('/home', (req, res, next) => {
     res.json('Welcome to user registration form!');
 });
 
--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,7 @@ import AuthRouter from './auth.js';
 import homeRouter from './home.js'
 export default function routes(app) {
     // Define your routes here
-    // Thêm route gốc để kiểm tra tình trạng server
+    app.use('/api', AuthRouter)
 
-    app.use('/api/auth', AuthRouter)
-    app.use('/api', (req, res) => {
-        res.send('API is working');
-    });
     app.use('/', homeRouter);
 }
